f4a66a74bc39c55d275574124c32d58dbff7302c
Alejandro Alonso <21010777+Alexalonso04@users.noreply.github.com>
2020-04-15 14:00:02 -0400

    Updates

--- src/constants/routes.js
+++
@@ -1,9 +0,0 @@
-export const LANDING = '/';
-export const SIGN_UP = '/signup';
-export const SIGN_IN = '/signin';
-export const HOME = '/home';
-export const ACCOUNT = '/account';
-export const PASSWORD_FORGET = '/pw-forget';
-export const RECOGNIZER = '/recognizer';
-export const NEWUSER = '/newuser';
-export const USERPANEL = '/userpanel';
